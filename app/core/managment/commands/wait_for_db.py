4185311a46f2895cb97ab20db79730d72aae971e
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-10 07:11:07 -0300

    :wrench: chore: Adicionado imagem do postgres ao container docker no app Recipe

---
+++ app/core/managment/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+""""
+Django commando to wait for the database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
