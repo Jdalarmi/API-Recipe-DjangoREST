3da177d4ea15177af5d40e53f9cd45abffb5d580
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-11 06:51:50 -0300

    :wrench: chore: Teste de conexão ao PostgreSQL, e comando para verficar conexão

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+""""
+Django commando to wait for the database to be available
+"""
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        """Entrypoint for command"""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Databases unavailable, waiting for 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database Available!!'))
