a36accc5e6cd088de8dc4de4f9de53698ced475b
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-13 12:24:43 -0300

    :wrench: chore: Configuradando a model de usuario no projeto

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
+"""
+Database models
+"""
 from django.db import models
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Manager for users
+    """
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user"""
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Üser in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
