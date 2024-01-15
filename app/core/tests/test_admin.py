b6e1e2a0338c17247f68c012a91fe948587c27ac
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-15 13:57:16 -0300

     :test_tube: test: Criado teste para admin do Django

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,34 @@
+"""
+Test for Django admin modifications.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSitetests(TestCase):
+    """Tests from Django Admin"""
+
+    def setUp(self):
+        """Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='user@example.com',
+            password = 'testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user_login@example.com',
+            password = 'testpass123',
+            name = 'Test User',
+        )
+
+    def test_users_list(self):
+        """Test that users ate listed on pages"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
