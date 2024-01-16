19208fdd998d5f23729253f44219858e9dc5dc28
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-16 19:23:30 -0300

    Setup Test

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
-    'drf_spectacular'
+    'drf_spectacular',
+    'user',
 
 ]
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 """
 Test for models.
 """
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
