48d87c58c460024a5ba7401276d3a43aca8b607c
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-17 17:34:13 -0300

    :sparkles: feat: Criado novos metodos de geração de token na API e opção de ediçao do usuario da API

--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,30 @@
 """
 Views for the user API
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerilaizer
+from user.serializers import (
+    UserSerilaizer,
+    AuthTokenSerializer,
+    )
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerilaizer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the autheticated user."""
+    serializer_class = UserSerilaizer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrive and return the authenticated user"""
+        return self.request.user
