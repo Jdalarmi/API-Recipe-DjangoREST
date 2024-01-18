df9e908141fcef16ff0b059f83bc6ca9c2f47d3d
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-18 18:34:46 -0300

    Test: Testando criação de objetos no modelo de Receita

---
+++ app/recipe/serializers.py
@@ -0,0 +1,14 @@
+"""
+Serializers for recipe API's
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipes."""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+"""Url mappings for the recipe APIs."""
+
+from django.urls import (
+    path, include,
+)
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
