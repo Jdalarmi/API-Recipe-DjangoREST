275ec88933611338e53aa46c98ed721e46ae3bbc
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-19 11:28:02 -0300

    Test: Novo teste para obeter detalhes da receita

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -12,3 +12,9 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
