aa0e87bc5e10a459c14081c17ff714e321f96a09
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-23 17:36:07 -0300

    Configurando volumes para armazenar as imagens com Pillow

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,10 +1,13 @@
 
-from django.contrib import admin
-from django.urls import include, path
 from drf_spectacular.views import(
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,3 +20,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
