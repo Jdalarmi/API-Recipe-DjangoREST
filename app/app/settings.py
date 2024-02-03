130de0d93c07b376a4651e0bd79381826ea05720
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-02-03 11:33:44 -0300

    Configrações deploy AWS

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+tgm+lv=b*p!82bm=o4v-%@x0kni)20%!e0#e=pu-d$ffu(dc'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
--- proxy/default.conf.tpl
+++ proxy/default.conf.tpl
@@ -2,12 +2,12 @@ server {
     listen ${LISTEN_PORT};
 
     location /static {
-        alias /vol/static
+        alias /vol/static;
     }
 
     location / {
         uwsgi_pass              ${APP_HOST}:${APP_PORT};
-        include                 /ect/nginx/uwsgi_params;
+        include                 /etc/nginx/uwsgi_params;
         client_max_body_size    10M;
     }
 }
