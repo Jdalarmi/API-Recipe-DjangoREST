4140f67f64e6779e94b3c380bb81138cb9fbdb70
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-02-03 14:26:28 -0300

    :tests: Finalizar o ultimo teste da API de receitas

--- app/app/urls.py
+++ app/app/urls.py
@@ -9,8 +9,11 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api_health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
